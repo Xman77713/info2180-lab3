5edff7ce79182351afecf92ebef75e9e9cef65a9
Pierre Mannix <pierre.mannix@mymona.uwi.edu>
2022-10-07 13:35:05 -0500

    Exercise 4 - Check for the winner and update the status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,32 +1,69 @@
 let load = function() {
-	let gameState = ["","","","","","","","",""]
-	let turnCounter = 0
+	let currentGameState = ["","","","","","","","",""];
+	let turnCounter = 0;
 	
 	function clicker(clicky, boxIndex) {
 		if (turnCounter % 2 == 0 && clicky.innerHTML == "") {
 			clicky.classList.add("X");
 			clicky.innerHTML = "X";
-			gameState[boxIndex] = "X";
+			currentGameState[boxIndex] = "X";
 			turnCounter += 1;
-			console.log(gameState + "\n" + turnCounter);
+			console.log(currentGameState + "\n" + turnCounter);
 		}
 		else if (turnCounter % 2 == 1 && clicky.innerHTML == "") {
 			clicky.classList.add("O");
 			clicky.innerHTML = "O";
-			gameState[boxIndex] = "O";
+			currentGameState[boxIndex] = "O";
 			turnCounter += 1;
-			console.log(gameState + "\n" + turnCounter);
+			console.log(currentGameState + "\n" + turnCounter);
+		}
+		
+		if (winCondition(currentGameState) == "X") {
+			document.getElementById("status").classList.add("you-won");
+			document.getElementById("status").innerHTML = "Congratulations! X is the Winner";
+		}
+		if (winCondition(currentGameState) == "O") {
+			document.getElementById("status").classList.add("you-won");
+			document.getElementById("status").innerHTML = "Congratulations! O is the Winner";
+		}
+	}
+	
+	function winCondition(gameState) {
+		if (gameState[0] == gameState[1] && gameState[0] == gameState[2]){
+			return gameState[0];
+		}
+		else if (gameState[3] == gameState[4] && gameState[3] == gameState[5]){
+			return gameState[3];
+		}
+		else if (gameState[6] == gameState[7] && gameState[6] == gameState[8]){
+			return gameState[6];
+		}
+		else if (gameState[0] == gameState[3] && gameState[0] == gameState[6]){
+			return gameState[0];
+		}
+		else if (gameState[1] == gameState[4] && gameState[1] == gameState[7]){
+			return gameState[1];
+		}
+		else if (gameState[2] == gameState[5] && gameState[2] == gameState[8]){
+			return gameState[2];
+		}
+		else if (gameState[0] == gameState[4] && gameState[0] == gameState[8]){
+			return gameState[0];
+		}
+		else if (gameState[2] == gameState[4] && gameState[2] == gameState[6]){
+			return gameState[2];
+		}
+		else {
+			return "";
 		}
 	}
 	
 	function hoverer(hovery) {
-		hovery.classList.add("hover")
-		console.log("HOVERED OVER SQUARE")
+		hovery.classList.add("hover");
 	}
 	
 	function outer(outy) {
-		outy.classList.remove("hover")
-		console.log("LEFT THE SQUARE")
+		outy.classList.remove("hover");
 	}
 	
 	let divList = document.getElementById("board").getElementsByTagName("div");
@@ -35,10 +72,12 @@ let load = function() {
 	for (let i = 0; i < 9; i++) {
 		let each = divList[i]
 		each.classList.add("square");
-		each.onmouseover = function() {hoverer(each)}
-		each.onmouseout = function() {outer(each)}
-		each.onclick = function() {clicker(each, i)}
+		each.onmouseover = function() {hoverer(each)};
+		each.onmouseout = function() {outer(each)};
+		each.onclick = function() {clicker(each, i)};
 	}
+	
+	
 }
 
 window.addEventListener("load", load);
