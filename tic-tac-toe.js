0f5a15f214095879d125188eb8bdecb99f0bb17e
Pierre Mannix <pierre.mannix@mymona.uwi.edu>
2022-10-07 12:29:27 -0500

    Exercise 3 - Change the style when you move your mouse over a square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,26 +8,37 @@ let load = function() {
 			clicky.innerHTML = "X";
 			gameState[boxIndex] = "X";
 			turnCounter += 1;
-			console.log(gameState.toString() + turnCounter.toString());
+			console.log(gameState + "\n" + turnCounter);
 		}
 		else if (turnCounter % 2 == 1 && clicky.innerHTML == "") {
 			clicky.classList.add("O");
 			clicky.innerHTML = "O";
 			gameState[boxIndex] = "O";
 			turnCounter += 1;
-			console.log(gameState.toString() + turnCounter.toString());
+			console.log(gameState + "\n" + turnCounter);
 		}
 	}
 	
-	let divList = document.querySelectorAll("#board > div");
-	let i = 0;
-	while (i <= 8) {
-		divList.item(i).classList.add("square");
-		i = i + 1;
+	function hoverer(hovery) {
+		hovery.classList.add("hover")
+		console.log("HOVERED OVER SQUARE")
 	}
-	divList.forEach((elem, index) => {
-		elem.onclick = function() {clicker(elem, index)};
-	});
+	
+	function outer(outy) {
+		outy.classList.remove("hover")
+		console.log("LEFT THE SQUARE")
+	}
+	
+	let divList = document.getElementById("board").getElementsByTagName("div");
+	console.log(divList);
+	
+	for (let i = 0; i < 9; i++) {
+		let each = divList[i]
+		each.classList.add("square");
+		each.onmouseover = function() {hoverer(each)}
+		each.onmouseout = function() {outer(each)}
+		each.onclick = function() {clicker(each, i)}
+	}
 }
 
 window.addEventListener("load", load);
