63493ac89e1e737e44860455b6bc41675f5a80d1
Pierre Mannix <pierre.mannix@mymona.uwi.edu>
2022-10-07 11:28:27 -0500

    Exercise 2 - Add an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,25 +1,21 @@
 let load = function() {
 	let gameState = ["","","","","","","","",""]
+	let turnCounter = 0
 	
 	function clicker(clicky, boxIndex) {
-		if (clicky.innerHTML == "") {
-			clicky.classList.remove("O")
+		if (turnCounter % 2 == 0 && clicky.innerHTML == "") {
 			clicky.classList.add("X");
 			clicky.innerHTML = "X";
 			gameState[boxIndex] = "X";
-			console.log(gameState);
+			turnCounter += 1;
+			console.log(gameState.toString() + turnCounter.toString());
 		}
-		else if (clicky.innerHTML == "X") {
-			clicky.classList.remove("X")
+		else if (turnCounter % 2 == 1 && clicky.innerHTML == "") {
 			clicky.classList.add("O");
 			clicky.innerHTML = "O";
 			gameState[boxIndex] = "O";
-			console.log(gameState);
-		}
-		else {
-			clicky.innerHTML = "";
-			gameState[boxIndex] = "";
-			console.log(gameState);
+			turnCounter += 1;
+			console.log(gameState.toString() + turnCounter.toString());
 		}
 	}
 	
@@ -29,25 +25,9 @@ let load = function() {
 		divList.item(i).classList.add("square");
 		i = i + 1;
 	}
-	let box1 = divList.item(0);
-	let box2 = divList.item(1);
-	let box3 = divList.item(2);
-	let box4 = divList.item(3);
-	let box5 = divList.item(4);
-	let box6 = divList.item(5);
-	let box7 = divList.item(6);
-	let box8 = divList.item(7);
-	let box9 = divList.item(8);
-	
-	box1.onclick = function() {clicker(box1, 0)};
-	box2.onclick = function() {clicker(box2, 1)};
-	box3.onclick = function() {clicker(box3, 2)};
-	box4.onclick = function() {clicker(box4, 3)};
-	box5.onclick = function() {clicker(box5, 4)};
-	box6.onclick = function() {clicker(box6, 5)};
-	box7.onclick = function() {clicker(box7, 6)};
-	box8.onclick = function() {clicker(box8, 7)};
-	box9.onclick = function() {clicker(box9, 8)};
+	divList.forEach((elem, index) => {
+		elem.onclick = function() {clicker(elem, index)};
+	});
 }
 
 window.addEventListener("load", load);
